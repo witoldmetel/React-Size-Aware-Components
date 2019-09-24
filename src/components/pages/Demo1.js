efb3f8d449671d5d97cae9b11e15a41832ebe578
witoldmetel <witoldmetel@gmail.com>
2019-09-24 20:35:44 +0200

    ref callback measure

--- src/components/pages/Demo1.js
+++ src/components/pages/Demo1.js
@@ -1,18 +1,57 @@
 import React, { Component } from "react";
-import { Card } from "antd";
+import { Card, Button } from "antd";
 
 import { AppContent } from "../AppContent";
 
 import "./Demo1.less";
 
 export class Demo1 extends Component {
+  state = { size: {}, isSizeChange: false };
+
+  componentDidUpdate() {
+    if (this.state.isSizeChange) {
+      this.getSize(this.elementRef.getBoundingClientRect());
+      this.setState({ isSizeChange: false });
+    }
+  }
+
+  refCallback = element => {
+    if (element) {
+      this.elementRef = element;
+      this.getSize(element.getBoundingClientRect());
+    }
+  };
+
+  getSize = size => {
+    this.setState({ size });
+  };
+
+  onResize = () => {
+    this.setState({ isSizeChange: true });
+    console.log(this.state.size);
+  };
+
+  get resizeButton() {
+    return (
+      <Button
+        type="primary"
+        size="small"
+        shape="circle"
+        icon="swap"
+        title="Resize Me!"
+        onClick={this.onResize}
+      />
+    );
+  }
+
   get content() {
     return (
-      <Card className="demo-card" title="Size Aware Component">
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-      </Card>
+      <div className="demo-card" ref={this.refCallback}>
+        <Card title="Size Aware Component" extra={this.resizeButton}>
+          <p>{`width: ${this.state.size.width} px`}</p>
+          <p>{`height: ${this.state.size.height} px`}</p>
+        </Card>
+      </div>
     );
   }
 
