63ff4ef9d9ed145cbb221fedfc042df540dcf16c
witoldmetel <witoldmetel@gmail.com>
2019-09-29 21:29:47 +0200

    sizeme

--- src/components/pages/Demo3.js
+++ src/components/pages/Demo3.js
@@ -1,57 +1,50 @@
 import React, { Component } from "react";
 import { Card } from "antd";
-import faker from "faker";
+import { SizeMe } from "react-sizeme";
 
 import { AppContent } from "../AppContent";
 
 export class Demo3 extends Component {
-  state = {
-    size: {},
-    cardData: this.fakeData
-  };
-
-  refCallback = element => {
-    if (element) {
-      this.elementRef = element;
-    }
-  };
-
-  componentDidMount() {
-    const observer = new ResizeObserver(entries => {
-      entries.forEach(entry => {
-        this.getSize(entry.contentRect);
-      });
-    });
-
-    observer.observe(this.elementRef);
-  }
-
   get fakeData() {
-    return faker.lorem.paragraphs(Math.random() * 5);
+    return (
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    );
   }
-
-  getSize = size => {
-    this.setState({ size });
-  };
 
   get content() {
     return (
-      <div className="content">
-        <div className="card-size">
-          <strong>contentRect</strong>
-          <p>Size: </p>
-          <pre>{JSON.stringify(this.state.size, null, 2)}</pre>
-        </div>
-        <div className="second demo-card" ref={this.refCallback}>
-          <Card title="Resize Me!">
-            <p>{this.state.cardData}</p>
-          </Card>
-        </div>
-      </div>
+      <SizeMe
+        monitorHeight
+        refreshRate={32}
+        render={({ size }) => (
+          <div className="content">
+            <div className="card-size">
+              <p>Size: </p>
+              <pre>{JSON.stringify(size, null, 2)}</pre>
+            </div>
+            <div className="second demo-card" ref={this.refCallback}>
+              <Card title="Resize Me!">{this.fakeData}</Card>
+            </div>
+          </div>
+        )}
+      />
     );
   }
 
   render() {
-    return <AppContent title="Demo 3" content={this.content} />;
+    return (
+      <AppContent
+        title="A React component has to know the size of its container"
+        content={this.content}
+      />
+    );
   }
 }
